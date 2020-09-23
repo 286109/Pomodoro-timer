e78a7991939554b2d31cf6d26ea31ea2974aee70
286109 <286109@gmail.com>
2020-09-23 14:10:58 +0400

    CHANGE: рефакторинг

--- pomodoro.py
+++ pomodoro.py
@@ -11,13 +11,12 @@ class Clock():
             'Helvetica', 48),  foreground="red", background="white")
         self.label = ttk.Label(text="", style='BW.TLabel')
         self.label.pack()
-        self._pomodoro_time_limit = 10
+        self._pomodoro_time_limit = 20 * 60
         self._pomodoro_starttime = time.time()
         self.start_button = ttk.Button(text='Start', command=self.start_timer)
         self.start_button.pack()
         self.progressbar = ttk.Progressbar()
         self.progressbar.pack()
-        self.root.mainloop()
 
     def update_clock(self):
         now = int(time.time() - self._pomodoro_starttime)
@@ -25,18 +24,19 @@ class Clock():
         if now > self._pomodoro_time_limit:
             self.root.after_cancel(self._timer_job)
             return
-        
-        self.label.configure(text='Прошло\n' + str(now) + ' секунд')
+
+        self.label.configure(
+            text=f'Прошло\n {now // 60} минут\n{now % 60} секунд')
         # Запуск таймера
         self._timer_job = self.root.after(1000, self.update_clock)
         self.progressbar.step(self._progressbar_increment)
-        
 
     def start_timer(self):
         self._pomodoro_starttime = time.time()
-        self._progressbar_increment = 100 / self._pomodoro_time_limit 
+        self._progressbar_increment = 100 / self._pomodoro_time_limit
         self.progressbar['value'] = -self._progressbar_increment
         self.update_clock()
 
-
-app = Clock()
+if __name__ == "__main__":
+    app = Clock()
+    app.root.mainloop()
